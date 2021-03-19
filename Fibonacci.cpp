9812da87348748ffb90c14c274391b16ebbbbbd5
Miguel Castrog <micastrog@unal.edu.co>
2021-03-19 14:14:47 +0000

    Secuencia de Fibonacci

---
+++ Fibonacci.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+void fibo (int x, int y, int l, int suma);
+
+int main(int argc, char **argv) {
+ 
+ int a=1;
+ int b=2;
+ int limit=500;
+ int sum;
+ fibo (a,b,limit,sum);
+
+  
+  return 0;
+}
+
+void fibo (int x,int y,int l, int suma=0) {
+
+while (x+y<l){
+suma=x+y;
+x=y;
+y=suma;
+
+std::cout << y<<"\n";
+}
+std::cout << "El resultado final es:" <<"\n"<< y <<"\n";
+}
+
+
+   
+   
