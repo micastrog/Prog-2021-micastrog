128800d8ce168561dbfeabddf0d91a1c19054671
Miguel Castro <micastrog@unal.edu.co>
2021-03-24 03:26:42 +0000

    Numeros primos hasta X numero

---
+++ Primos.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+void loop(int num, int exit, int div);
+
+int main(void){
+
+  int newnum=100;
+  int newexit;
+  int newdiv;
+
+  loop(newnum,newexit,newdiv);
+
+
+}
+
+void loop(int num, int exit, int div){
+
+  for(int ii = num ; ii>1 ;ii=ii-1){
+
+    exit=0;
+    div=2;
+    while(exit!=1){
+      
+      if(ii%div==0){exit=1;}
+      else{ div=div +1;}
+
+    }
+    
+    if(div==ii){ std::cout<< ii << "\n"; }
+
+  }
+
+}
+
+
